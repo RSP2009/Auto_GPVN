5226d5ef1fdf7cdf67160a8b8a80548a433aa1ff
RSP2009 <122031231+RSP2009@users.noreply.github.com>
2024-01-04 19:31:30 +0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,35 +1,40 @@
-"""For backward compatibility, expose main functions from
-``setuptools.config.setupcfg``
-"""
-import warnings
-from functools import wraps
-from textwrap import dedent
-from typing import Callable, TypeVar, cast
+#
+# Copyright (C) 2009-2020 the sqlparse authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of python-sqlparse and is released under
+# the BSD License: https://opensource.org/licenses/BSD-3-Clause
 
-from .._deprecation_warning import SetuptoolsDeprecationWarning
-from . import setupcfg
+from sqlparse.filters.others import SerializerUnicode
+from sqlparse.filters.others import StripCommentsFilter
+from sqlparse.filters.others import StripWhitespaceFilter
+from sqlparse.filters.others import SpacesAroundOperatorsFilter
 
-Fn = TypeVar("Fn", bound=Callable)
+from sqlparse.filters.output import OutputPHPFilter
+from sqlparse.filters.output import OutputPythonFilter
 
-__all__ = ('parse_configuration', 'read_configuration')
+from sqlparse.filters.tokens import KeywordCaseFilter
+from sqlparse.filters.tokens import IdentifierCaseFilter
+from sqlparse.filters.tokens import TruncateStringFilter
 
+from sqlparse.filters.reindent import ReindentFilter
+from sqlparse.filters.right_margin import RightMarginFilter
+from sqlparse.filters.aligned_indent import AlignedIndentFilter
 
-def _deprecation_notice(fn: Fn) -> Fn:
-    @wraps(fn)
-    def _wrapper(*args, **kwargs):
-        msg = f"""\
-        As setuptools moves its configuration towards `pyproject.toml`,
-        `{__name__}.{fn.__name__}` became deprecated.
+__all__ = [
+    'SerializerUnicode',
+    'StripCommentsFilter',
+    'StripWhitespaceFilter',
+    'SpacesAroundOperatorsFilter',
 
-        For the time being, you can use the `{setupcfg.__name__}` module
-        to access a backward compatible API, but this module is provisional
-        and might be removed in the future.
-        """
-        warnings.warn(dedent(msg), SetuptoolsDeprecationWarning, stacklevel=2)
-        return fn(*args, **kwargs)
+    'OutputPHPFilter',
+    'OutputPythonFilter',
 
-    return cast(Fn, _wrapper)
+    'KeywordCaseFilter',
+    'IdentifierCaseFilter',
+    'TruncateStringFilter',
 
-
-read_configuration = _deprecation_notice(setupcfg.read_configuration)
-parse_configuration = _deprecation_notice(setupcfg.parse_configuration)
+    'ReindentFilter',
+    'RightMarginFilter',
+    'AlignedIndentFilter',
+]
